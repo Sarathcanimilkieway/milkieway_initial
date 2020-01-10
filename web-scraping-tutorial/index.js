a0d7e29fe08ebb0156f30ec2b0c75e35d2014251
Sarath C Ani <mailsforsarath98@gmail.com>
2020-01-11 01:38:05 +0530

    title scraped

--- web-scraping-tutorial/index.js
+++ web-scraping-tutorial/index.js
@@ -1,4 +1,35 @@
 const request = require('request')
-const cheerio = require('cheerio')
-const table = requrie('cli-table')
+const fetch = require('node-fetch')
 
+const cheerio = require('cheerio')
+
+
+const url_first = "https://www.imdb.com/find?q="
+const url_last = "&s=tt&ttype=ft&ref_=fn_ft"
+
+// url = url_first + term + url_last
+
+searchMovie = (term) => {
+    url = url_first + term.replace(' ' , '%20') + url_last
+    
+    // console.log(url)
+    return fetch(url).then(response => response.text())
+
+}
+
+let movie_list = []
+
+searchMovie('star wars')
+    .then((response) => {
+        // console.log(response)
+
+        const $ = cheerio.load(response)
+        $(".result_text").each(function( index , element) {
+            let movie = $(element).text()
+
+            movie_list.push(movie.trim())
+        })
+        
+
+        console.log(movie_list)
+    })
